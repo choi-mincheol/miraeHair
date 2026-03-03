abe0bb50ced29397406a6f421b4ae31ea0774d74
cminc <20071937@vision.hoseo.edu>
2026-03-03 18:42:49 +0900

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/mirae/hair/HairPjtApplication.java
@@ -0,0 +1,13 @@
+package com.mirae.hair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairPjtApplication.class, args);
+	}
+
+}
